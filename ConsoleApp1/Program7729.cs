995b0a440ebd612d2b6de5b7753d391a35f767b5
noa123715 <noa123715@gmail.com>
2022-10-23 16:52:45 +0300

    stage 0

---
+++ ConsoleApp1/Program7729.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Targil0
+{
+   partial class Program
+    {
+        static void Main(string[] arg)
+        {
+            Welcome7729();
+            Welcome0796();
+            Console.ReadKey();
+        }
+        private static void Welcome0796() { }
+        private static void Welcome7729()
+        {
+            Console.WriteLine("enter yourjhjhj name");
+            string userName = Console.ReadLine();
+            Console.WriteLine("{0} welcome to our first console", userName);
+        }
+    }
+}
