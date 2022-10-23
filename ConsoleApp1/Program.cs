324770adac2e3bd2698ba83a9b48cad0c3360025
noa123715 <noa123715@gmail.com>
2022-10-23 16:45:26 +0300

    jkjkjkj

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+namespace Targil0
+{
+    class Program
+    {
+        static void Main(string[] arg)
+        {
+
+            Console.WriteLine("enter yourjhjhj name");
+            string userName= Console.ReadLine();
+            Console.WriteLine("{0} welcome to our first console",userName);
+            Console.ReadKey();
+        }
+    }
+}
