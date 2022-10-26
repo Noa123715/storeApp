9b402e2f0faa342a5e5f16f7c3b44d1d6c4ece00
shoshimor <smorgenstern46@outlook.co.il>
2022-10-26 12:50:49 +0300

    stage o- final

--- stage0/Program7729.cs
+++ stage0/Program7729.cs
@@ -10,10 +10,15 @@ namespace Targil0
             Welcome0796();
             Console.ReadKey();
         }
-        private static void Welcome0796() { }
+        private static void Welcome0796() {
+            Console.WriteLine("noa! kol hakavod! eich korim lachotech?");
+            string sister = Console.ReadLine();
+            Console.WriteLine(" noa, tari le{0} eize yofi!", sister);
+
+        }
         private static void Welcome7729()
         {
-            Console.WriteLine("enter yourjhjhj name");
+            Console.WriteLine("enter your name");
             string userName = Console.ReadLine();
             Console.WriteLine("{0} welcome to our first console", userName);
         }
