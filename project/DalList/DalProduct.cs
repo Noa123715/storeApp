b86d27983c2a2a7607751f55005db863e9495107
noa123715 <noa123715@gmail.com>
2022-11-20 16:20:10 +0200

    DalOrder & DalOrderItem & DalProduct in List

--- project/DalList/DalProduct.cs
+++ project/DalList/DalProduct.cs
@@ -4,6 +4,7 @@
 /// reading the existing product,
 /// updating product and deletions.
 /// </summary>
+
 using Dal.DO;
 namespace DalList;
 
@@ -17,56 +18,36 @@ public struct DalProduct
 
     public static Product ReadProduct(int id)
     {
-        for (int i = 0; i < DataSource.Config.productIdx; i++)
+        foreach (Product item in DataSource.productList)
         {
-            if (DataSource.productList[i].ID == id)
+            if (item.ID == id)
             {
-                return DataSource.productList[i];
+                return item;
             }
         }
-        throw new System.Exception("The product was not found in the list");
+        throw new Exception("The product was not found in the list");
     }
 
-    public static Product[] ReadProduct()
+    public static List<Product> ReadProduct()
     {
-        Product[] newProductList = new Product[DataSource.Config.productIdx];
-        for (int i = 0; i < DataSource.Config.productIdx; i++)
-        {
-            newProductList[i] = DataSource.productList[i];
-        }
+        List<Product> newProductList = new List<Product>();
+        newProductList.AddRange(DataSource.productList);
         return newProductList;
     }
 
     public static void DeleteProduct(int id)
     {
-        for (int i = 0; i < DataSource.Config.productIdx - 1; i++)
-        {
-            if (DataSource.productList[i].ID == id)
-            {
-                DataSource.productList[i] = DataSource.productList[DataSource.Config.productIdx];
-                DataSource.Config.productIdx--;
-                return;
-            }
-        }
-        if (DataSource.productList[DataSource.Config.productIdx].ID == id)
-        {
-            DataSource.Config.productIdx--;
-            return;
-        }
-        throw new System.Exception("The product was not found in the list");
+        DataSource.productList.RemoveAll(item => item.ID == id);
+        //throw new Exception("The product was not found in the list");
     }
 
     public static void UpDateProduct(Product UpProduct)
     {
-        for (int i = 0; i < DataSource.Config.productIdx; i++)
+        int index = DataSource.productList.FindIndex(item => item.ID == UpProduct.ID);
+        if (index == -1)
         {
-            if (DataSource.productList[i].ID == UpProduct.ID)
-            {
-                DataSource.productList[i] = UpProduct;
-                return;
-            }
+            throw new Exception("The product was not found in the list");
         }
-        throw new System.Exception("The product was not found in the list");
     }
 }
 
