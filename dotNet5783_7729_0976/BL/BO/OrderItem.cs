74a21ad4a885ae1fe736b7a98b1f4ac5358f109e
shoshimor <smorgenstern46@outlook.co.il>
2022-11-29 21:07:40 +0200

    finish BO entities

---
+++ dotNet5783_7729_0976/BL/BO/OrderItem.cs
@@ -0,0 +1,26 @@
+﻿/// <summary>
+/// 
+/// </summary>
+
+namespace BO
+{
+    public class OrderItem
+    {
+        public int ID { get; set; }
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+        public double TotalPrice { get; set; }
+
+
+        //overriding ToString method prints the orderItem properties.
+        public override string ToString() => 
+            $@" ID: {ID}, 
+            ProductName: {ProductName},
+            ProductID: {ProductID}
+           , Amount: {Amount},  
+             Price: {Price}, 
+             TotalPrice: {TotalPrice}";
+    }
+}
---
+++ dotNet5783_7729_0976/BL/BO/Product.cs
@@ -0,0 +1,18 @@
+﻿/// <summary>
+/// Product class contains the prodacts properties and toString method.
+/// </summary>
+
+namespace BO;
+
+    public class Product
+    {
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public eCategories Category { get; set; }
+    public int InStock { get; set; }
+
+    //overriding ToString method enables printing the Product properties.
+    public override string ToString() => $@"Product ID: {ID}, Name: {Name},Price: {Price}, Category: {Category},  Instock: {InStock}";
+}
+
