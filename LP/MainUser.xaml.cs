96d7779dc1a610cadc5fab3cec62d0de3cc2bd5a
noa123715 <noa123715@gmail.com>
2022-12-11 16:00:17 +0200

    wpf begin

---
+++ LP/MainUser.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LP
+{
+    /// <summary>
+    /// Interaction logic for MainUser.xaml
+    /// </summary>
+    public partial class MainUser : Window
+    {
+        public MainUser()
+        {
+            InitializeComponent();
+        }
+    }
+}
