1b10bc015fd6c7ebb657a0b4d6fdf24e9fd152f3
shoshimor <smorgenstern46@outlook.co.il>
2022-12-13 18:03:54 +0200

    product list view

--- LP/MainWindow.xaml.cs
+++ LP/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace LP;
 
@@ -20,13 +7,18 @@ namespace LP;
 /// </summary>
 public partial class MainWindow : Window
 {
+    private BlApi.IBL Bl { get; set; }
+
     public MainWindow()
     {
         InitializeComponent();
+        Bl = new BlImplementation.BL();
     }
 
     private void ChooseProductBtnWindow_Click(object sender, RoutedEventArgs e)
     {
-
+        ProductList productListWindow = new();
+        productListWindow.Show();
+        this.Hide();
     }
 }
