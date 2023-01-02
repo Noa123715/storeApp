4814a63efde87955c4b306a4174d1bb00b388c80
shoshimor <smorgenstern46@outlook.co.il>
2023-01-02 15:00:19 +0200

    implenent dalxml order

--- DalXml/Order.cs
+++ DalXml/Order.cs
@@ -4,15 +4,56 @@ using DO;
 using System;
 using System.Collections.Generic;
 using System.Xml.Linq;
-
+/// <summary>
+/// 
+/// </summary>
 internal class Order : IOrder
 {
-    private List<Order> orderList { get; set; }
+    private List<DO.Order> orderList { get; set; }
     public Order()
     {
-        XElement root = XDocument.Load("../../xml/Order.xml").Root;
-        Order order = new();
-        orderList = new List<Order>();
+        XElement? root = XDocument.Load(@"../../xml/Order.xml")?.Root;
+        DO.Order newOrder = new ();
+        orderList = new List<DO.Order>();
+
+        foreach (var xmlOrder in root?.Elements("Order"))
+        {
+            newOrder.ID = Convert.ToInt32(xmlOrder.Element("ID")?.Value);
+            newOrder.CustomerName = xmlOrder?.Element("CustomerName")?.Value ;
+            newOrder.CustomerEmail = xmlOrder?.Element("CustomerEmail")?.Value;
+            newOrder.CustomerAddress = xmlOrder?.Element("CustomerAddress")?.Value;
+            newOrder.OrderDate = Convert.ToDateTime(xmlOrder.Element("OrderDate")?.Value);
+            newOrder.ShipDate = Convert.ToDateTime(xmlOrder.Element("ShipDate")?.Value);
+            newOrder.DeliveryDate = Convert.ToDateTime(xmlOrder.Element("DeliveryDate")?.Value);
+            orderList.Add(newOrder);
+        }
+
+    }
+
+    public void Create(DO.Order newOrder)
+    {
+        XElement? rootConfig = XDocument.Load(@"../../xml/config.xml").Root;
+
+        XElement ?id = rootConfig?.Element("orderID");
+        int orderId = Convert.ToInt32(id?.Value);
+        orderId++;
+        id.Value= orderId.ToString();
+        rootConfig?.Save(@"../../xml/config.xml");
+        newOrder.ID= orderId;
+        orderList.Add(newOrder);
+       
+        XElement orderXML = new("Order",
+                        new XElement("ID", newOrder.ID),
+                        new XElement("CustomerName", newOrder.CustomerName),
+                        new XElement("CustomerEmail", newOrder.CustomerEmail),
+                        new XElement("CustomerAddress", newOrder.CustomerAddress),
+                        new XElement("OrderDate", newOrder.OrderDate),
+                        new XElement("ShipDate", newOrder.ShipDate),
+                        new XElement("DeliveryDate", newOrder.DeliveryDate));
+        
+
+
+
 
     }
     //public int Create(DO.Order order)
@@ -44,10 +85,7 @@ internal class Order : IOrder
     //{
 
     //}
-    public int Create(DO.Order item)
-    {
-      
-    }
+
 
     public void Delete(int id)
     {
