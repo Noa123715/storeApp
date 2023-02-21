7df2b3ff31327c0300bec4cdb86564729af2d5bf
noa123715 <noa123715@gmail.com>
2023-02-21 11:32:08 +0200

    order track

--- LP/order/OrderTrackingWindow.xaml.cs
+++ LP/order/OrderTrackingWindow.xaml.cs
@@ -7,9 +7,20 @@ namespace PL;
 /// </summary>
 public partial class OrderTrackingWindow : Window
 {
-    public OrderTrackingWindow(OrderTracking orderTrack)
+    public OrderTrackingWindow(Order orderTrack)
     {
         InitializeComponent();
         IdOrderText.Text = orderTrack.ID.ToString();
+        NameCustomerText.Text = orderTrack.CustomerName?.ToString();
+        EmailCustomerText.Text = orderTrack.CustomerEmail?.ToString();
+        AddressCustomerText.Text = orderTrack.CustomerAddress?.ToString();
+        OrderDateText.Text = orderTrack.OrderDate?.ToString();
+        ShipDateText.Text = orderTrack.ShipDate?.ToString();
+        DeliveryDateText.Text = orderTrack.DeliveryDate?.ToString();
+    }
+
+    private void OrderDetails_Click(object sender, RoutedEventArgs e)
+    {
+
     }
 }
